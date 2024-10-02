f659241bc3698de400d5541f3680cb4212cbe37b
T1murKO <kutsenko.timur@gmail.com>
2024-10-02 20:18:40 +0300

    Fix ssl context

--- better_imap/proxy.py
+++ better_imap/proxy.py
@@ -12,13 +12,18 @@ class IMAP4_SSL_PROXY(IMAP4_SSL):
         self._proxy = proxy
         super().__init__(*args, **kwargs)
 
-    def create_client(self, host: str, port: int, loop: asyncio.AbstractEventLoop,
-                      conn_lost_cb: Callable[[Optional[Exception]], None] = None,
-                      ssl_context: ssl.SSLContext = None):
+    def create_client(
+        self,
+        host: str,
+        port: int,
+        loop: asyncio.AbstractEventLoop,
+        conn_lost_cb: Callable[[Optional[Exception]], None] = None,
+        ssl_context: ssl.SSLContext = None,
+    ):
         local_loop = loop if loop is not None else get_running_loop()
 
         if ssl_context is None:
-            ssl_context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+            ssl_context = ssl.create_default_context()
 
             # В Украине нет доступа к Рамблеру без этих настроек
             ssl_context.check_hostname = False
@@ -31,7 +36,9 @@ class IMAP4_SSL_PROXY(IMAP4_SSL):
         async def create_connection():
             proxy = AsyncProxy.from_url(self._proxy.as_url)
             sock = await proxy.connect(dest_host=host, dest_port=port)
-            await local_loop.create_connection(lambda: self.protocol, sock=sock, ssl=ssl_context, server_hostname=host)
+            await local_loop.create_connection(
+                lambda: self.protocol, sock=sock, ssl=ssl_context, server_hostname=host
+            )
 
         self.protocol = IMAP4ClientProtocol(local_loop, conn_lost_cb)
         local_loop.create_task(create_connection())
